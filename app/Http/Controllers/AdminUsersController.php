c210ad0b652193389cf3f5e7aae9651e8ea868ef
Samue <thedamilare@gmail.com>
2018-03-28 18:27:35 +0100

    update user working

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserEditRequest;
 use App\Http\Requests\UsersRequest;
 use App\Photo;
 use App\Role;
@@ -52,7 +53,17 @@ class AdminUsersController extends Controller
     {
         //
 
-        $input = $request->all();
+        if(trim($request->password) == ''){
+
+            $input = $request->except('password');
+
+        } else {
+
+            $input = $request->all();
+
+            $input['password'] = bcrypt($request->password);
+
+        }
 
         if($file = $request->file('photo_id')){
 
@@ -65,8 +76,6 @@ class AdminUsersController extends Controller
             $input['photo_id'] = $photo->id;
 
         }
-
-        $input['password'] = bcrypt($request->password);
 
         User::create($input);
 
@@ -100,7 +109,11 @@ class AdminUsersController extends Controller
     {
         //
 
-        return view('admin.users.edit');
+        $user = User::findOrFail($id);
+
+        $roles = Role::lists('name','id')->all();
+
+        return view('admin.users.edit', compact('user', 'roles'));
 
     }
 
@@ -111,9 +124,42 @@ class AdminUsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UserEditRequest $request, $id)
     {
         //
+
+        $user = User::findOrFail($id);
+
+        if(trim($request->password) == ''){
+
+            $input = $request->except('password');
+
+        } else {
+
+            $input = $request->all();
+
+            $input['password'] = bcrypt($request->password);
+
+        }
+
+        if($file = $request->file('photo_id')){
+
+            $name = time() . $file->getClientOriginalName();
+
+            $file->move('images', $name);
+
+            $photo = Photo::create(['file'=>$name]);
+
+            $input['photo_id'] = $photo->id;
+
+        }
+
+        $user->update($input);
+
+        return redirect('/admin/users');
+
+
+
     }
 
     /**
--- app/Photo.php
+++ app/Photo.php
@@ -10,4 +10,12 @@ class Photo extends Model
 
     protected $fillable = ['file'];
 
+    protected $uploads = '/images/';
+
+    public function getFileAttribute($photo){
+
+        return $this->uploads . $photo;
+
+    }
+
 }
