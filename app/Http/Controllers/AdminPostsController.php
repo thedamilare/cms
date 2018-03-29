b0c93e10c046680dab38335fa256373897968657
Samue <thedamilare@gmail.com>
2018-03-30 00:43:49 +0100

    Able to create and view posts

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use App\Http\Requests\PostCreateRequest;
 use App\Photo;
 use App\Post;
@@ -36,7 +37,9 @@ class AdminPostsController extends Controller
     {
         //
 
-        return view('admin.posts.create');
+        $categories = Category::lists('name','id')->all();
+
+        return view('admin.posts.create', compact('categories'));
 
     }
 
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -27,7 +27,7 @@
           <tr>
             <td>{{$post->id}}</td>
             <td>{{$post->user->name}}</td>
-            <td>{{$post->category_id}}</td>
+            <td>{{$post->category->name}}</td>
             <td><img src="{{$post->photo ? $post->photo->file : 'http://placehold.it/400x400'}}" alt="" height="50"></td>
             <td>{{$post->title}}</td>
             <td>{{$post->body}}</td>
